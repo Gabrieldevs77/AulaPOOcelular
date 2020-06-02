4cfb890e99974c92b148e4230f2b5b6eb891684a
Gabrieldevs77 <petollolmel@gmail.com>
2020-06-02 14:14:29 -0300

    fimPOOcelular

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AulaPOOcelular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Aluno Breno = new Aluno();
+            Breno.nome = "Breno";
+            Breno.idade = 16;
+            Breno.bolsista = false;
+            Breno.valorMensalidade = 2000f;
+            Breno.mediaFinal = 9.7f;
+
+            Console.WriteLine("Breno: "+Breno.VerMediaFinal());
+            Console.WriteLine("Breno: "+Breno.VerMensalidade());
+            
+
+    }
+    }
+}
